ce8c53cc1da3a0d3ed1be19aa213d4e48776fcce
Rhys Woodberry <rhysbrian@gmail.com>
2018-07-10 20:03:00 +1000

    add test for navcomponent

--- eric-frontend/src/components/NavComponent.test.js
+++ eric-frontend/src/components/NavComponent.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import NavComponent from './NavComponent';
 
-it('renders without crashing'), () => {
+it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<NavComponent />, div);
   ReactDOM.unmountComponentAtNode(div);
-};
+});
