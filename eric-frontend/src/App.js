6350344cc0994c0b2128a4821014ce46e3b058ad
punya <punyachapagain@gmail.com>
2018-07-11 13:26:17 +1000

    fixed file path for caraousel

--- eric-frontend/src/App.js
+++ eric-frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import NavComponent from './components/NavComponent';
 import FooterComponent from './components/FooterComponent';
-import CarouselComponent from './CarouselComponent';
+import CarouselComponent from './components/CarouselComponent';
 import './App.css';
 
 class App extends Component {
