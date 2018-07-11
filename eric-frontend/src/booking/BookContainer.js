e92a122bd7f948c96747077875dad64f49a52213
Samuel Razali <samuelrazali@gmail.com>
2018-07-11 13:01:18 +1000

    Revert "Revert "please revert""
    This reverts commit 4485e7fb4b69cc682b24086e15d29c9710fe9ac7.

---
+++ eric-frontend/src/booking/BookContainer.js
@@ -0,0 +1,13 @@
+import {BookCal} from './BookCal'
+import {BookInfo} from './BookInfo'
+
+
+export const BookContainer () {
+    render(){
+        return(
+            <BookCal/>
+            <BookInfo/>
+        );
+    }
+}
+
