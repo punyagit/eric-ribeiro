26dc9705c33de0e81311a1b8407e949bc4c69059
Samuel Razali <samuelrazali@gmail.com>
2018-07-11 13:18:05 +1000

    Revert "completed calendar"
    This reverts commit 07d2aa2b57b7cd4db447061a6b0f47dd5edd8451.

--- eric-frontend/src/App.js
+++ eric-frontend/src/App.js
@@ -1,72 +1,31 @@
 import React, { Component } from 'react';
+//import logo from './logo.svg';
 import './App.css';
+//import ReactDOM from 'react-dom';
 import Calendar from 'react-calendar-material';
-import Display from './booking/booking_display';
-import { Button , Jumbotron} from 'reactstrap';
 
 class App extends Component {
-
-  constructor(props){
-    super(props);
-    this.state = {
-      day: "Day Placeholder",
-      month: "Month Placeholder",
-      year: "Year Placeholder",
-      timeslots: ["Timeslot 1","Timeslot 2"]
-    }
-
-    // binding "this" so the property won't be lost when passed
-    this.changeBooking = this.changeBooking.bind(this);
-  }
-
-  changeBooking (day,month,year,timeslots) {
-    this.setState({
-      day: day,
-      month: month,
-      year: year,
-      timeslots: timeslots
-     })
-
-    // to see the data that has been passed....
-      console.log("the state has been changed to:")
-      console.log(day)
-      console.log(month)
-      console.log(year)
-      console.log(timeslots)
-  }
-
   render() {
-    
-    let day_time1 = Math.floor(Math.random()*12) + ':00 AM'
-    let day_time2 = Math.floor(Math.random()*12) + ':00 AM'
-    let eve_time1 = Math.floor(Math.random()*12) + ':00 PM'
-    let eve_time2 = Math.floor(Math.random()*12) + ':00 PM'
-    let eve_time3 = Math.floor(Math.random()*12) + ':00 PM'
- 
-    const newTimeslots = [
-      day_time1,day_time2,eve_time1,eve_time2,eve_time3
-    ]
     return (
       <div className="App">
     <Calendar
-      accentColor={'blue'}
-      orientation={'flex-col'}
-      showHeader={false}
-      onDatePicked={(d) => {
-        var date = new Date(d);
-        var year = date.getFullYear();
-        var month = date.getMonth() + 1
-        var day = date.getDate();
-
-        this.changeBooking(day,month,year,newTimeslots);
+    accentColor={'red'}
+    orientation={'flex-col'}
+    showHeader={false}
+    onDatePicked={(d) => {
+      var date = new Date(d);
+      var day = date.getDate();
+      
+      var year = date.getFullYear();
+      var month = date.getMonth() + 1
+      //var time = date.getHours()
+      var arr = [year,month,day]
+      console.log("month:", month);
+      console.log("day:", day);
+      console.log(year)
+      //console.log(time)
+      console.log(arr)
     }}/>
-
-    <Display
-    day={this.state.day}
-    month={this.state.month}
-    year={this.state.year}
-    timeslots={this.state.timeslots}
-    />
 
       
      </div>
@@ -76,6 +35,4 @@ class App extends Component {
 
 
 
-
 export default App;
-
--- eric-frontend/src/index.js
+++ eric-frontend/src/index.js
@@ -6,4 +6,3 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
-
