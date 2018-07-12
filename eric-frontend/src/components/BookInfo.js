83081d06e1c3e37eab08c118d14a57354055a5b7
Samuel Razali <samuelrazali@gmail.com>
2018-07-13 02:13:41 +1000

    massive improvement on UI and cleaner code

--- eric-frontend/src/components/BookInfo.js
+++ eric-frontend/src/components/BookInfo.js
@@ -1,34 +1,24 @@
 import React, { Component } from 'react';
 import BookCal from '../pages/BookCal';
-import {CardBody, Card} from 'reactstrap';
+import {Button, CardBody, Card, Form, Label, Input} from 'reactstrap';
+
+
 class BookInfo extends Component {
     constructor(props) {
         super(props);
-        this.state = {value: ''};
-    
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-      }
-    
-      handleChange(event) {
-        this.setState({value: event.target.value});
-      }
-    
-      handleSubmit(event) {
-        alert('A name was submitted: ' + this.state.value);
-        event.preventDefault();
+        this.onSubmit = this.onSubmit.bind(this);
       }
 
-    
+      onSubmit(e){
+        e.preventDefault();
+        this.props.onSubmit();
+        console.log(this.state)
+      }
+      
     render(){
         const style = {
-            // border: '20 solid black',
             background: "linear-gradient(rgba(120, 120, 120, 120) 1%, rgb(147, 147, 147) 0%, rgba(0, 0, 0, 0) 100%)",
             padding: 20,
-        }
-        const style2 = {
-            backgroundColor: 'grey',
-            padding: 30
         }
 
         let day = this.props.day;
@@ -36,33 +26,40 @@ class BookInfo extends Component {
         let year = this.props.year;
         let timeslots = this.props.timeslots;
         let duration = this.props.duration
+
         return (
         <div>
         
           <Card>
-            <CardBody>
-            <p>Day : {day} | Month : {month} | Year : {year}</p>
-                <p>Timeslots :</p>
-                <ul>           
-                    {this.props.timeslots.map(x => <button>{x}</button>)}
-                </ul>
-                {/* <form onSubmit={(event) => { handleSubmit(event, onLogIn) }}> */}
-                <form>
+
+                <CardBody style={style}>
+                    
+                    <Form>
+                    <Label for="day">Day</Label>
+                    <Input value={day} name="day" readOnly/>
+                    <Label for="month">Month</Label>
+                    <Input value={month} name="month" readOnly/>
+                    <Label for="year">Year</Label>
+                    <Input value={year} name="year" readOnly/>
+                    <Label for="duration">Duration</Label>
+                    <Input value={duration} name="duration" readOnly/>
+                    <Label for="timeslots">Timeslots</Label>
+                    <Input type="select" name="timeslots">
+                            {timeslots.map(x => <option>{x}</option>)}
+                    </Input>
+                    <Button color="info" onClick={e => this.onSubmit(e)}>Submit!</Button>
+                </Form >
+                </CardBody>
+
+                <CardBody>
+                    <h2>Product Description</h2>
+                    <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
+                </CardBody>
+
+            </Card>
+
+
             
-                    <input defaultValue={day} name="day" readOnly/>
-                    <input defaultValue={month} name="month" readOnly/>
-                    <input defaultValue={year} name="year" readOnly/>
-                    <input defaultValue={duration} name="duration" readOnly/>
-                    <button type="submit" value="Submit"/>
-                </form >
-            </CardBody>
-              </Card>
-
-
-            <div style={style2}>
-                <h2>Product Description</h2>
-                <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
-            </div>
 
         </div>
         )
--- eric-frontend/src/pages/BookCal.js
+++ eric-frontend/src/pages/BookCal.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import BookInfo from '../components/BookInfo';
 import Calendar from 'react-calendar-material';
 import NavComponent from '../components/NavComponent';
 import FooterComponent from '../components/FooterComponent';
+import BookInfo from '../components/BookInfo';
 import {Collapse, Jumbotron, Button} from 'reactstrap';
 
 
@@ -10,33 +10,42 @@ class BookCal extends Component {
   constructor(props){
     super(props);
     this.state = {
-      timeslots: ["Timeslot 1","Timeslot 2"],
+      day: "",
+      month: "",
+      year: "",
+      timeslots: ["timeslot placeholder"],
+      duration: "",
       collapse: false
     }
 
-    // binding "this" so the property won't be lost when passed
     this.changeBooking = this.changeBooking.bind(this);
     this.toggle = this.toggle.bind(this)
   }
 
 
   toggle() {
-    // this.setState({ collapse: !this.state.collapse });
     this.setState({ collapse: true });
   }
 
-  changeBooking (day,month,year,duration) {
+  changeBooking (day,month,year,timeslots,duration) {
     this.setState({
       day: day,
       month: month,
       year: year,
-      duration:duration,
-      
+      timeslots: timeslots,
+      duration: duration
      })
+     console.log(this.state)
+  }
+
+  onSubmit = e => {
+    console.log("Passed value: ", e)
   }
 
   render() {
-       let arr = ["time1","time2","time3"]
+    // test assign new timeslots array
+       let arr = ["timeslot 1","timeslot 2","timeslot 3", "timeslot 4", "timeslot 5"]
+       
     return (
       <div>
     <NavComponent />
@@ -46,14 +55,14 @@ class BookCal extends Component {
       orientation={'flex-col'}
       showHeader={false}
       onDatePicked={(d) => {
-        var date = new Date(d);
-        var year = date.getFullYear();
-        var month = date.getMonth() + 1;
-        var day = date.getDate();
-        var duration= 7;
-        
-        this.toggle;
-        this.changeBooking(day,month,year,duration);
+        let date = new Date(d);
+        let year = date.getFullYear();
+        let month = date.getMonth() + 1;
+        let day = date.getDate();
+        let timeslots = arr;
+        let duration = "duration placeholder";
+
+        this.changeBooking(day,month,year,timeslots,duration);
         
     }}/>
     </Button>
@@ -61,12 +70,12 @@ class BookCal extends Component {
     
     <Collapse isOpen={this.state.collapse}>
         <BookInfo
-        day={this.state.day}
-        month={this.state.month}
-        year={this.state.year}
+        day= {this.state.day}
+        month= {this.state.month}
+        year= {this.state.year}
         timeslots={this.state.timeslots}
-        duration = {this.state.duration}
-        
+        duration= {this.state.duration}
+        onSubmit= {e => this.onSubmit(e)}
         />
 
     </Collapse>
