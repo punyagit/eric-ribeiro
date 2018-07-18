7b83ce0bbbc472be5a32c1d43537368f2fd5bfe6
Samuel Razali <samuelrazali@gmail.com>
2018-07-18 13:24:21 +1000

    finish signup functionality

--- eric-frontend/src/components/SignUpForm.js
+++ eric-frontend/src/components/SignUpForm.js
@@ -1,33 +1,60 @@
 import React, { Component } from "react";
-import { Button, FormGroup, Input, Label } from "reactstrap";
-// source : https://serverless-stack.com/chapters/create-a-login-page.html
+import { Form, Button, FormGroup, Input, Label } from "reactstrap";
 export default class SignUpForm extends Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      username: "",
+      password: "",
+      name: "",
+      email: "",
+      phnumber: ""
+      };
+      
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    
+    handleChange(e) {
+      this.setState(
+        {[e.target.name]: e.target.value}
+      );
+    }
+    
+    handleSubmit(e) {
+      let log = [];
+      Object.entries(this.state).forEach(([key, value]) => {
+        log.push(`${key} = ${value} \n`);
+      });
+      alert('You have submitted :\n' + log);
+      e.preventDefault();
+    }
+    
+    render() {
+      return (
+        <Form onSubmit={this.handleSubmit}>
+        <FormGroup>
+        <Label> User Name </Label>
+        <Input type="text" name="username" onChange={e => this.handleChange(e)} />
+        </FormGroup>
+        <FormGroup>
+        <Label> Password </Label>
+        <Input type="password" name="password" onChange={e => this.handleChange(e)} />
+        </FormGroup>
+        <FormGroup>
+        <Label> Name </Label>
+        <Input type="text" name="name" onChange={e => this.handleChange(e)} />
+        </FormGroup>
+        <FormGroup>
+        <Label> Email </Label>
+        <Input type="text" name="email" onChange={e => this.handleChange(e)} />
+        </FormGroup>
+        <FormGroup>
+        <Label> Phone Number </Label>
+        <Input type="text" name="phnumber" onChange={e => this.handleChange(e)} />
+        </FormGroup>
+        <Button type="submit">Submit </Button>
+        </Form>
+      );
+    }
   }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
-    event.preventDefault();
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    );
-  }
-}
