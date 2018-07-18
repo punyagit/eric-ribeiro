f29e4e5f9033f139e0d0b499b06d01822176d7c4
Samuel Razali <samuelrazali@gmail.com>
2018-07-18 10:55:32 +1000

    begin working on signup form

--- eric-frontend/src/App.js
+++ eric-frontend/src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Route, NavLink, Redirect } from 'react-router-
 import './App.css';
 import HomePage from './pages/HomePage';
 import BookCal from './pages/BookCal';
+import ContactUs from './pages/ContactUs';
+import SignUpPage from './pages/SignUpPage';
+
 class App extends Component {
   render() {
     return (
@@ -10,8 +13,8 @@ class App extends Component {
         <div>
           <Route exact path='/' component={HomePage} />
           <Route exact path='/bookings' component={BookCal} />
-
-          
+          <Route exact path='/contactus' component={ContactUs} />
+          <Route exact path='/signup' component={SignUpPage} />
         </div>
       </Router>
     
