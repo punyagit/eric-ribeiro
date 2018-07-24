b76b85b1e7d7454392cc6c1268831f29e4cdc965
Rhys Woodberry <rhysbrian@gmail.com>
2018-07-24 17:21:09 +1000

    fix react routes

--- eric-frontend/src/App.js
+++ eric-frontend/src/App.js
@@ -32,26 +32,6 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <div className="app">
-          <NavComponent />
-
-          
-          <Route exact path='/' component={CarouselComponent} />
-          <Route exact path='/about' component={About} />
-          <Route exact path='/booking/:serviceName/:serviceDuration/:servicePrice' render={
-              () => <BookCal />
-            }/>
-          <Route exact path='/signup' component={SignUpForm} />
-          <Container fluid>
-          <Route exact path='/services' render={
-              () => <Services services={this.state.services} />
-            }/>
-            </Container>
-    
-          <FooterComponent />
-        
-        </div>
-
           <main className="mainWindow">
             <Switch>
               <Route exact path='/' component={CarouselComponent} />
@@ -67,10 +47,8 @@ class App extends Component {
             </Switch>
           </main>
           <FooterComponent />
-          </div>
       </Router>
       
-    
     );
   }
 }
