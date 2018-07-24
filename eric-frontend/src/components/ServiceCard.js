8396fccd0f0526ffa018331c7907ab15a86c6456
samfoong <samfoong88@gmail.com>
2018-07-24 16:01:49 +1000

    Pulling new code

--- eric-frontend/src/components/ServiceCard.js
+++ eric-frontend/src/components/ServiceCard.js
@@ -27,7 +27,7 @@ class ServiceCard extends React.Component {
     
         <Card>
           <CardBody>
-            <CardImg top width="100%" src={this.state.service.image} alt="Card image cap" />
+            <CardImg top width="100%" img src={"https://www.google.com.au/imgres?imgurl=http%3A%2F%2Fwww.modelhideout.com%2Fwordpress%2Fwp-content%2Fuploads%2F2014%2F06%2FWhat-is-a-Hair-Model.jpg&imgrefurl=http%3A%2F%2Fwww.modelhideout.com%2Fwhat-is-a-hair-model%2F&docid=7i7UoEIax-2-eM&tbnid=kOvLZ-NqcE3JqM%3A&vet=10ahUKEwjZ6e74hrfcAhUpEqYKHRo9DUMQMwjUASgCMAI..i&w=600&h=420&safe=off&bih=748&biw=866&q=hair%20model&ved=0ahUKEwjZ6e74hrfcAhUpEqYKHRo9DUMQMwjUASgCMAI&iact=mrc&uact=8"} alt="Card image cap" />
             <hr/>
             <CardTitle>{this.state.service.name}</CardTitle>
             <CardSubtitle className="cardsubtitle">Duration: {this.state.service.duration} hours | Price: ${this.state.service.price}</CardSubtitle>
