b4a3fde9498ea9ea8f2565baad4036c60799ac8e
Rhys Woodberry <rhysbrian@gmail.com>
2018-07-24 17:45:59 +1000

    fix router errors

--- eric-frontend/src/App.js
+++ eric-frontend/src/App.js
@@ -31,27 +31,24 @@ class App extends Component {
 
   render() {
     return (
-      <Router>
-        <div className="app">
-          <NavComponent />
-          <main className="mainWindow">
-            <Switch>
-              <Route exact path='/' component={CarouselComponent} />
-              <Route exact path='/about' component={About} />
-              <Route exact path='/booking/:serviceName/:serviceDuration/:servicePrice' render={
-                  () => <BookCal />
-                }/>
-              <Route exact path='/signup' component={SignUpForm} />
-              <Route exact path='/services' render={
-                  () => <Services services={this.state.services} />
-                }/>
-              <Route component={NoMatch} />
-            </Switch>
-          </main>
-          <FooterComponent />
-        </div>
-      </Router>
-      
+      <div>
+        <NavComponent />
+        <main className="mainWindow">
+          <Switch>
+            <Route exact path='/' component={CarouselComponent} />
+            <Route exact path='/about' component={About} />
+            <Route exact path='/booking/:serviceName/:serviceDuration/:servicePrice' render={
+                () => <BookCal />
+              }/>
+            <Route exact path='/signup' component={SignUpForm} />
+            <Route exact path='/services' render={
+                () => <Services services={this.state.services} />
+              }/>
+            <Route component={NoMatch} />
+          </Switch>
+        </main>
+        <FooterComponent />
+      </div>
     );
   }
 }
