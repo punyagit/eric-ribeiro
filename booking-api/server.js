a1a9c361f4e312959e4699ff5b3f93f912bce9a1
Rhys Woodberry <rhysbrian@gmail.com>
2018-07-24 15:04:37 +1000

    add express to server.js

--- booking-api/server.js
+++ booking-api/server.js
@@ -10,7 +10,7 @@ server.listen(PORT, console.log(PORT));
 
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  express().use(express.static("client/build"));
+  express().use(express.static("eric-frontend/build"));
   // Routes | Create the routes. Any route that gets hit here, load the react index.html file.
   express().get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "eric-frontend", "build", "index.html"));
