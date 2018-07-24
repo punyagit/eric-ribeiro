3d8e3bad74730017e980c478a14f5d0c10f8bd2d
Rhys Woodberry <rhysbrian@gmail.com>
2018-07-24 15:32:37 +1000

    express request app.js

--- booking-api/server.js
+++ booking-api/server.js
@@ -1,4 +1,3 @@
-const express = require('express');
 const http = require('http');
 const app = require('./app');
 require('dotenv').load();
@@ -7,12 +6,3 @@ const PORT = process.env.PORT || 8081;
 const server = http.createServer(app);
 
 server.listen(PORT, console.log(PORT));
-
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  express().use(express.static("eric-frontend/build"));
-  // Routes | Create the routes. Any route that gets hit here, load the react index.html file.
-  express().get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "eric-frontend", "build", "index.html"));
-  });
-}
